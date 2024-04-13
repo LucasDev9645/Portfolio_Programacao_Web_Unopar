5206c9e684594ca84277a3ff0f1f14a129a2278a
LucasDev <LucasDev9645@gmail.com>
2024-04-13 11:39:23 -0300

    Projeto finalizado

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.findById(id).ifPresent(userRepository::delete);
+    }
+
+    public User update(Long id, User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+}
